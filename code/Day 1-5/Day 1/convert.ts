3b0059e644a2d16ea2279768cd29cf34075abc40
henrychris <64085210+henrychris@users.noreply.github.com>
2023-07-30 11:47:09 +0100

    add files for day 1

---
+++ code/Day 1-5/Day 1/convert.ts
@@ -0,0 +1,21 @@
+// Sample function that adds two numbers
+function addNumbers(a, b) {
+	return a + b;
+}
+
+// Sample function that greets a person
+function greetPerson(name) {
+	return `Hello, ${name}!`;
+}
+
+// Sample data
+const num1 = 5;
+const num2 = 10;
+const personName = "John Doe";
+
+// Sample usage
+const result = addNumbers(num1, num2);
+console.log("Result:", result);
+
+const greeting = greetPerson(personName);
+console.log(greeting);
